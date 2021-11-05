143508281f6a8c20d3cce41d02757e16b641f1e6
Kartar <kartar.jabanda@missguided.com>
2021-11-05 16:12:34 +0000

    complete UI with dummy data

--- src/pages/ProfilePage.tsx
+++ src/pages/ProfilePage.tsx
@@ -1,10 +1,17 @@
 import React from "react";
-import { Container, Box, Stack, Avatar, Heading, Text } from "@chakra-ui/react";
+import { Container, Box, Stack, Avatar, Heading, Text, Link } from "@chakra-ui/react";
+import { InfoIcon, TriangleUpIcon, AtSignIcon } from "@chakra-ui/icons";
 
 const dummyData = {
   name: "Kartar",
   username: "misskartar",
-  bio: "Software Engineer @MissGuided React Native."
+  bio: "Software Engineer @MissGuided React Native.",
+  company: "Missguided",
+  location: "Notts",
+  twitter: {
+    username: "kartarjabanda",
+    link: 'https://twitter.com/kartarjabanda'
+  }
 };
 
 const ProfilePage = () => {
@@ -17,16 +24,29 @@ const ProfilePage = () => {
         maxW="3xl"
         borderRadius="lg"
       >
-        <Stack direction="row" justifyContent='space-between' marginBottom='3'>
+        <Stack direction="row" justifyContent="space-between" marginBottom="3">
           <Box>
             <Heading size="lg">{dummyData.name}</Heading>
-            <Text marginBottom='2'>@{dummyData.username}</Text>
+            <Text marginBottom="2">@{dummyData.username}</Text>
             <Text>{dummyData.bio}</Text>
           </Box>
           <Avatar src="https://bit.ly/broken-link" />
         </Stack>
 
-        
+        <Stack direction="row" alignItems="center">
+          <InfoIcon />
+          <Text>{dummyData.company}</Text>
+        </Stack>
+        <Stack direction="row" alignItems="center">
+          <TriangleUpIcon />
+          <Text>{dummyData.location}</Text>
+        </Stack>
+        <Stack direction="row" alignItems="center">
+          <AtSignIcon />
+          <Link href={dummyData.twitter.link} target='_blank'>
+            <Text>{dummyData.twitter.username}</Text>
+          </Link>
+        </Stack>
       </Box>
     </Container>
   );
